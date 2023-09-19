b6b1ecd45ae6bb4c3f68aafbbc90bc72df0e2f53
elizabethseto <elizabethwseto@gmail.com>
2023-09-19 13:31:26 -0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 
 export default defineConfig({
-  base:"/elizabethseto",
+  base:"/",
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 2000,
